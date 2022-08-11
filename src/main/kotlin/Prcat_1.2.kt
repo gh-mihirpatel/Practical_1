00b055d6090da9da528e7abe7ea411ca8cf45340
mihir <mihirpatelmp10@gmail.com>
2022-08-10 23:27:15 -0700

    Question 2

---
+++ src/main/kotlin/Prcat_1.2.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var int = 10
+    val double: Double = int.toDouble()
+    print(double)
+
+    var string = "123"
+    val int1 : Int = string.toInt()
+    println(int1)
+
+    var double: Double = string.toDouble()
+    println(double)
+
+
+
+}
